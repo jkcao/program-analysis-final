f58f68dba9f0dfe972d02a51c597726878800151
Jan Orlowski <jan.orlowsk@gmail.com>
2020-05-06 23:41:32 -0400

    testing checks file sizes now

--- test.py
+++ test.py
@@ -1,9 +1,39 @@
-import modifydeterministic
+import modifydeterministic as mdet
+import os
+import re
+import subprocess
 
 
 def main():
     modifyFilePath = "/home/jan/coreutils/src/ln.c"
     testFilePath = "/home/jan/coreutils/tests/ln"
-    modifydeterministic.startModify(modifyFilePath, testFilePath)
+
+    # Run make to build all the files (wanna make sure object file exists)
+    makeCommand = 'cd /home/jan/coreutils; make'
+    try:
+        subprocess.check_output(makeCommand, shell=True, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError:
+        print("Failed to make initial source. Aborting.")
+        return
+    
+    # Get the size of the object file before changes 
+    oldSize = getCompiledByteSize(modifyFilePath)
+
+    # Make the changes
+    mdet.startModify(modifyFilePath, testFilePath)
+
+    # Get the size of the object file after changes and compare both
+    newSize = getCompiledByteSize(modifyFilePath)
+    print("")
+    print("---------------------------------------------------------------------------------")
+    print("Old Size= " + str(oldSize) + " bytes")
+    print("New Size= " + str(newSize) + " bytes")
+    print("Reduced file size by " + str(oldSize - newSize) + " bytes")
+    print("---------------------------------------------------------------------------------")
+    print("")
+
+def getCompiledByteSize(sourcePath):
+    objPath = re.sub("\.c",".o",sourcePath)
+    return os.path.getsize(objPath)
 
 main()
